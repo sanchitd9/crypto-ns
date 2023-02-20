88cdd804241dd43febf9483f756d75d6ee06f3c3
Sanchit Dass <sanchitd9@gmail.com>
2023-02-20 11:09:26 -0500

    removed object files and added commands

--- lab5/netfilter/.seedFilter.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/seed/crypto-ns/lab5/netfilter/seedFilter.mod := { echo  /home/seed/crypto-ns/lab5/netfilter/seedFilter.o;  echo; } > /home/seed/crypto-ns/lab5/netfilter/seedFilter.mod
--- lab5/netfilter/seedFilter.mod
+++
@@ -1,2 +0,0 @@
-/home/seed/crypto-ns/lab5/netfilter/seedFilter.o
-
